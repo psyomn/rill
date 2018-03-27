dfc507b23fb3a1d9c8146d2fff58898723d1a9c5
Simon Symeonidis <lethaljellybean@gmail.com>
2018-03-27 17:44:07 -0400

    fixup! Add rill stream

--- src/rill_stream.c
+++ src/rill_stream.c
@@ -1,5 +1,6 @@
 #include "rill.h"
 
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -17,9 +18,15 @@ int main(int argc, char* argv[]) {
     FILE* sout = freopen(NULL, "wb", stdout);
     struct rill_store* store = rill_store_open(argv[1]);
     struct rill_store_it *it = rill_store_begin(store);
-    size_t pairs_count = rill_store_pairs(store);
+    const size_t pairs_count = rill_store_pairs(store);
+
+    const rill_ts_t ts = rill_store_ts(store);
+    const size_t quant = rill_store_quant(store);
+
     struct rill_kv kv = {0};
 
+    fwrite(&ts, sizeof(ts), 1, sout);
+    fwrite(&quant, sizeof(quant), 1, sout);
     fwrite(&pairs_count, sizeof(pairs_count), 1, sout);
 
     while (rill_store_it_next(it, &kv)) {
@@ -28,7 +35,7 @@ int main(int argc, char* argv[]) {
     }
 
     fclose(sout);
+    rill_store_it_free(it);
     rill_store_close(store);
-
     return 0;
 }
